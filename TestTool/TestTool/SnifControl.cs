e5ae719d5de54bc7391ddf5dc8a91a8f83d64ae5
kien.nguyen.trung.79 <kien.nguyen.trung.79@gmail.com>
2012-05-25 08:42:50 +0000

    Add Sniffer Tab.

---
+++ TestTool/TestTool/SnifControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    partial class Test_Form
+    {
+        private void Load_Snif_Tab(object sender, SerialDataReceivedEventArgs e)
+        {
+            //SnifPort_Name.DataSource = Enum.GetValues(typeof(BarcodeType));
+            //SnifPort_Name.DataSource = SerialPort.GetPortNames();
+ 
+        }
+
+        private void Snif_receiveData(object sender, SerialDataReceivedEventArgs e)
+        {
+            int len, i;
+            SerialPort thisCom = (SerialPort)sender;
+            byte[] rxBuffer = new byte[BUF_LEN];
+
+            len = thisCom.BytesToRead >= BUF_LEN ? BUF_LEN : thisCom.BytesToRead;
+            thisCom.Read(rxBuffer, 0, len);       // Read Data from COMPORT
+
+            for (i = 0; i < len; i++)
+            {
+                PKB_rxCharEvent(rxBuffer[i]);
+            }
+        }
+    }
+}
